c92fbd738ea86c9b88de35d3c76f63e7d211288e
Trig Desktop <hamish@trig.nz>
2021-07-23 14:46:32 +1200

    Scroll Rect extendable pt1

---
+++ Assets/Scripts/Editor/RecyclableGridLayoutEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(RecyclableGridLayout))]
+[CanEditMultipleObjects]
+public class RecyclableGridLayoutEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+    }
+}
---
+++ Assets/Scripts/RecyclableGridLayout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecyclableGridLayout : GridLayoutGroup
+{
+    public ScrollRect scrollRect;
+
+    protected override void OnEnable()
+    {
+        scrollRect.onValueChanged.AddListener(onContentMove);
+    }
+
+    protected override void OnDisable()
+    {
+        
+    }
+
+    void onContentMove(Vector2 scrollPos)
+    {
+        Debug.Log(scrollPos);
+    }
+}
